b6e168d2be73d44d5db17d4f285288c6d0f326dc
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-07-14 00:09:20 -0300

    class euro

--- src/Euro.java
+++ src/Euro.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Euro {
+public class Euro extends Moeda {
+    public Euro(double initialValue) {
+        this.valor = initialValue;
+    }
+
+    @Override
+    public void info() {
+        System.out.print(" Euro - €" + valor);
+    }
+
+    @Override
+    public double converter() {
+        return this.valor * 5.39;
+    }
 }
