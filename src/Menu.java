cfd6aa079f498d8f5e3d8f83ba2b3849a742c6af
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-07-14 16:10:36 -0300

    att-poo menu

--- src/Menu.java
+++ src/Menu.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class Menu {
     private Scanner scanner;
+    private Cofrinho cofrinho;
 
     public Menu() {
         scanner = new Scanner(System.in);
+        cofrinho = new Cofrinho();
     }
     public void showMenu() {
         System.out.println("COFRINHO:");
-        System.out.println("1 - Adicionar moeda:");
+        System.out.println("1 - Adicionar moeda");
         System.out.println("2 - Remover moeda");
         System.out.println("3 - Listar moedas");
-        System.out.println("4 - Calcular total convertido para Real");
+        System.out.println("4 - Calcular valor total convertido para Real");
         System.out.println("0 - Encerrar");
 
         System.out.print("Escolha uma opção: ");
@@ -21,12 +23,88 @@ public class Menu {
             case "0":
                 System.out.println("Programa encerrado");
                 break;
+
             case "1":
+                showMenuAddCoins();
+                showMenu();
+                break;
+
+            case "2":
+                showMenuRemoveCoins();
+                showMenu();
+                break;
+
+            case "3":
+                cofrinho.listagemMoedas();
+                showMenu();
+                break;
+
+            case "4":
+                double totalConverted = cofrinho.totalConvertido();
+                System.out.println("Valor total convertido: R$" + totalConverted);
+                showMenu();
+                break;
 
             default:
                 System.out.println("Opção inválida!");
                 showMenu();
                 break;
+        }
+    }
+
+    private void showMenuAddCoins() {
+        System.out.println("Escolha uma moeda:");
+        System.out.println("1 - Real");
+        System.out.println("2 - Dólar");
+        System.out.println("3 - Euro");
+
+        System.out.print(">> ");
+        int option = scanner.nextInt();
+
+        System.out.print("Digite o valor: ");
+        double valueCoin = scanner.nextDouble();
+
+        if (option == 1) {
+            Real coin = new Real(valueCoin);
+            cofrinho.adicionar(coin);
+        } else if (option == 2) {
+            Dolar coin = new Dolar(valueCoin);
+            cofrinho.adicionar(coin);
+        } else if (option == 3) {
+            Euro coin = new Euro(valueCoin);
+            cofrinho.adicionar(coin);
+        } else {
+            System.out.println("Opção inválida!");
+            showMenu();
+        }
+
+        System.out.println("Moeda adicionada!");
+    }
+
+    private void showMenuRemoveCoins() {
+        System.out.println("Escolha uma moeda:");
+        System.out.println("1 - Real");
+        System.out.println("2 - Dólar");
+        System.out.println("3 - Euro");
+
+        System.out.print(">> ");
+        int option = scanner.nextInt();
+
+        System.out.print("Digite o valor: ");
+        double valueCoin = scanner.nextDouble();
+
+        if (option == 1) {
+            Real coin = new Real(valueCoin);
+            cofrinho.remover(coin);
+        } else if (option == 2) {
+            Dolar coin = new Dolar(valueCoin);
+            cofrinho.remover(coin);
+        } else if (option == 3) {
+            Euro coin = new Euro(valueCoin);
+            cofrinho.remover(coin);
+        } else {
+            System.out.println("Opção inválida!");
+            showMenu();
         }
     }
 }
