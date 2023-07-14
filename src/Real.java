8db11273d9db699677ce56488298b5ae5bbab238
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-07-14 16:10:23 -0300

    att-poo real

--- src/Real.java
+++ src/Real.java
@@ -5,11 +5,24 @@ public class Real extends Moeda {
 
     @Override
     public void info() {
-        System.out.print("Real - R$" + valor);
+        System.out.println("Real - R$" + valor);
     }
 
     @Override
     public double converter() {
         return this.valor;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        if (this.valor != ((Real) obj).valor) {
+            return false;
+        }
+
+        return true;
     }
 }
