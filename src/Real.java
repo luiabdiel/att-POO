da6f9f6459ce7a7c5c35e754ef920627d331208b
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-07-14 00:09:49 -0300

    class real

--- src/Real.java
+++ src/Real.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Real {
+public class Real extends Moeda {
+    public Real(double initialValue) {
+        this.valor = initialValue;
+    }
+
+    @Override
+    public void info() {
+        System.out.print("Real - R$" + valor);
+    }
+
+    @Override
+    public double converter() {
+        return this.valor;
+    }
 }
