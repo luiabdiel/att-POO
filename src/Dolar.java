fb1e1ec69fe2a0a6bf8934118d53bcf5ebad826a
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-07-14 00:09:09 -0300

    class dolar

--- src/Dolar.java
+++ src/Dolar.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Dolar {
+public class Dolar extends Moeda {
+
+    public Dolar(double initialValue) {
+        this.valor = initialValue;
+    }
+
+    @Override
+    public void info() {
+        System.out.print(" Dólar - $" + valor);
+    }
+
+    @Override
+    public double converter() {
+        return this.valor * 4.80;
+    }
 }
